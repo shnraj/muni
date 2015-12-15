1dca7bf8666f483a563997b5ec3bebe9f6cca4ab
Sahana Rajasekar <shnraj@gmail.com>
2015-12-15 01:16:56 -0800

    Clean up

--- muni.py
+++ muni.py
@@ -1,27 +1,36 @@
-import requests
-
-from xml.etree import ElementTree
 import config
+import requests
+from xml.etree import ElementTree
 
-token = config.token
-params = {'token': token, 'agencyName': 'SF-MUNI'}
-response = requests.get('http://services.my511.org/Transit2.0/GetRoutesForAgency.aspx', params=params)
-response_xml = ElementTree.fromstring(response.content)
 
-agency_list = response_xml.find('AgencyList')
-agency = agency_list.find("Agency")
-route_list = agency.find("RouteList")
+def main():
+    print "Welcome to the Muni Script!"
 
-all_routes = [route.get("Name") for route in list(route_list.iter("Route"))]
+    valid_route = False
+    all_routes = get_muni_routes()
+    while not valid_route:
+        route_answer = raw_input("Enter the muni route you want: ")
 
-print "Welcome to the Muni Script!"
+        if route_answer == "" or route_answer == "?":
+            print '\n'.join(all_routes)
+        elif route_answer in all_routes:
+            valid_route = True
+            print "Nice!"
 
-valid_route = False
-while not valid_route:
-    route_answer = raw_input("Enter the muni route you want: ")
 
-    if route_answer == "" or route_answer == "?":
-        print '\n'.join(all_routes)
-    elif route_answer in all_routes:
-        valid_route = True
-        print "Nice!"
+def get_muni_routes():
+    params = {'token': config.token, 'agencyName': 'SF-MUNI'}
+    response = requests.get('http://services.my511.org/Transit2.0/GetRoutesForAgency.aspx', params=params)
+    response_xml = ElementTree.fromstring(response.content)
+
+    agency_list = response_xml.find('AgencyList')
+    agency = agency_list.find("Agency")
+    route_list = agency.find("RouteList")
+
+    all_routes = [route.get("Name") for route in list(route_list.iter("Route"))]
+    return all_routes
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
