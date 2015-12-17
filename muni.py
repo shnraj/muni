b46fb3a1ce338cb69fc4cc989e193e3e389ba34a
Sahana Rajasekar <shnraj@gmail.com>
2015-12-16 18:10:40 -0800

    Clean up code, add comments

--- muni.py
+++ muni.py
@@ -1,6 +1,6 @@
-import config
-import requests
-from xml.etree import ElementTree
+import config  # file that contains my API token
+import requests  # used to make the API request
+from xml.etree import ElementTree  # used to parse XML
 
 
 def main():
@@ -11,26 +11,30 @@ def main():
     while not valid_route:
         route_answer = raw_input("Enter the muni route you want: ")
 
-        if route_answer == "" or route_answer == "?":
-            print '\n'.join(all_routes.keys())
-        elif route_answer in all_routes.keys():
+        if route_answer in all_routes.keys():
             valid_route = True
             print "Got it!"
             break
+        else:
+            print "Pick one of these: "
+            print ", ".join(all_routes.keys())
 
-    direction_answer = raw_input("Inbound or Outbound? ")
+    direction_answer = ""
+    while direction_answer not in ["Inbound", "Outbound"]:
+        direction_answer = raw_input("Inbound or Outbound? ")
 
     valid_stop = False
     all_stops = get_route_stops(all_routes[route_answer], direction_answer)
     while not valid_stop:
         stop_answer = raw_input("Enter the stop you want: ")
 
-        if stop_answer == "" or stop_answer == "?":
-            print '\n'.join(all_stops.keys())
-        elif stop_answer in all_stops.keys():
+        if stop_answer in all_stops.keys():
             valid_stop = True
             print "Got it!"
             break
+        else:
+            print "Pick one of these: "
+            print ", ".join(all_stops.keys())
 
     all_times = get_next_departures(all_stops[stop_answer])
     print "Next departure time for route " \
@@ -40,11 +44,13 @@ def main():
 
 
 def get_muni_routes():
+    """ Returns a dictionary of all muni routes mapped to their route codes """
+
     params = {'token': config.token, 'agencyName': 'SF-MUNI'}
     end_point = 'http://services.my511.org/Transit2.0/GetRoutesForAgency.aspx'
     response_xml = get_xml_response(end_point, params)
 
-    agency_list = response_xml.find('AgencyList')
+    agency_list = response_xml.find("AgencyList")
     agency = agency_list.find("Agency")
     route_list = agency.find("RouteList")
 
@@ -54,13 +60,15 @@ def get_muni_routes():
 
 
 def get_route_stops(route_code, direction):
+    """ Returns a dictionary of all muni stops, for a certain route and direction,
+    mapped to their stop codes """
+
     params = {'token': config.token,
               'routeIDF': 'SF-MUNI~' + route_code + '~' + direction}
     end_point = 'http://services.my511.org/Transit2.0/GetStopsForRoutes.aspx'
-
     response_xml = get_xml_response(end_point, params)
 
-    agency_list = response_xml.find('AgencyList')
+    agency_list = response_xml.find("AgencyList")
     agency = agency_list.find("Agency")
     route_list = agency.find("RouteList")
     route = route_list.find("Route")
@@ -74,12 +82,13 @@ def get_route_stops(route_code, direction):
 
 
 def get_next_departures(stop_code):
+    """ Returns a list of all departure times for a given stop """
+
     params = {'token': config.token, 'stopcode': stop_code}
     end_point = 'http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx'
-
     response_xml = get_xml_response(end_point, params)
 
-    agency_list = response_xml.find('AgencyList')
+    agency_list = response_xml.find("AgencyList")
     agency = agency_list.find("Agency")
     route_list = agency.find("RouteList")
     route = route_list.find("Route")
@@ -94,6 +103,9 @@ def get_next_departures(stop_code):
 
 
 def get_xml_response(end_point, params):
+    """ Given API end-point and params, function parses response
+    XML string and returns an Element object """
+
     response = requests.get(end_point, params=params)
     return ElementTree.fromstring(response.content)
 
